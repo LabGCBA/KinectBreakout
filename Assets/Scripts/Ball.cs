125b32eb80b0a4ec3ad01dc91e0dd2f18a4c2856
Hernán Patrón Costas <hernanpatroncostas@gmail.com>
2017-01-16 11:30:29 -0300

    Commit inicial

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float ballInitialVelocity = 600f;
+    private bool ballInPlay = false;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((Input.GetButtonDown("Fire1") || KinectManager.instance.isFire) && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+            KinectManager.instance.isFire = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+
+    public int lives = 3;
+    public int bricks = 16;
+    public float resetDelay = 1f;
+    public Text livesText;
+    public GameObject gameOver;
+    public GameObject youWon;
+    public GameObject bricksPrefab;
+    public GameObject paddle;
+    public GameObject deathParticles;
+    public static GM instance = null;
+    public GameObject buttonStart;
+    public GameObject buttonQuit;
+
+    private GameObject clonePaddle; 
+     
+	// Use this for initialization
+	void Start () {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+        //Setup();
+	}
+	
+    public void Setup()
+    {
+        buttonStart.SetActive(false);
+        buttonQuit.SetActive(false);
+        livesText.gameObject.SetActive(true);
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+        Instantiate(bricksPrefab, transform.position, Quaternion.identity);
+    }
+
+    void CheckGameOver()
+    {
+        if (bricks < 1)
+        {
+            youWon.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke("Reset", resetDelay);
+        }
+
+        if (lives < 1)
+        {
+            gameOver.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke("Reset", resetDelay);
+        }
+
+    }
+
+    public void StartGame()
+    {
+        Setup();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Reset()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+        livesText.text = "Lives: " + lives;
+        Instantiate(deathParticles, clonePaddle.transform.position, Quaternion.identity);
+        Destroy(clonePaddle);
+        Invoke("SetupPaddle", resetDelay);
+        CheckGameOver();
+    }
+
+    void SetupPaddle()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+    }
+
+    public void DestroyBrick()
+    {
+        bricks--;
+        CheckGameOver();
+    }
+}
